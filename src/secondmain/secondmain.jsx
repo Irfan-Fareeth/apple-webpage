3c9a71a37ac5518d93a85ef58a6ae1a7ba7635ba
Irfan-Fareeth <167504277+Irfan-Fareeth@users.noreply.github.com>
2024-12-31 19:04:48 +0530

    Update secondmain.jsx
    public update

--- src/secondmain/secondmain.jsx
+++ src/secondmain/secondmain.jsx
@@ -7,17 +7,17 @@ const SecondMain = () => {
     <section className='secondmain'>
        <div className='main'>
        <div className='cop'>
-          <SlideBlocks source1="../public/79dda01c-3271-4a2b-af65-eaa2d8f2dc54.jpeg"
-                       source2="../public/applepic.jpg"
-                       source3="../public/Apple accessories.jpeg"
+          <SlideBlocks source1="../79dda01c-3271-4a2b-af65-eaa2d8f2dc54.jpeg"
+                       source2="../applepic.jpg"
+                       source3="../Apple accessories.jpeg"
                        interval="3000"
                       />
 
        </div>
        <div className='cop'>
-          <SlideBlocks source1="../public/How Apple Vision Pro is Revolutionizing the Digital Experience_.jpeg"
-                       source2="../public/iphone-15-pro-blue-256gb-12.webp"
-                       source3="../public/stad.jpg"
+          <SlideBlocks source1="../How Apple Vision Pro is Revolutionizing the Digital Experience_.jpeg"
+                       source2="../iphone-15-pro-blue-256gb-12.webp"
+                       source3="../stad.jpg"
                        interval="2000"/>
        </div>
        </div>
